8d718b179be1bd1b566aa8ac95ac53648521c95b
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-02-25 05:25:12 +0000

    extract common component TopNav

---
+++ web/TopNav.svelte
@@ -0,0 +1,34 @@
+<script>
+  import SvgHome from "./svg/SvgHome.svelte";
+  import Twitter from "./icons/Twitter.svelte";
+  import GitHub from "./icons/GitHub.svelte";
+
+  export let gitHubURL = "https://github.com/kjk";
+</script>
+
+<div class="flex items-center mx-4 mb-2 pt-1 pb-2 text-gray-500">
+  <div class="flex font-bold text-lg items-center">
+    <a class="px-1 py-1 mr-1 hover:bg-gray-100" href="/">
+      <SvgHome />
+    </a>
+    <div class="mr-2">/</div>
+    <slot />
+  </div>
+  <div class="grow" />
+  <a
+    class="px-1 py-1 mr-1 hover:bg-gray-100"
+    href={gitHubURL}
+    target="_blank"
+    rel="noreferrer"
+  >
+    <GitHub />
+  </a>
+  <a
+    class="px-1 py-1 mr-2 hover:bg-gray-100"
+    href="https://twitter.com/kjk"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <Twitter />
+  </a>
+</div>
