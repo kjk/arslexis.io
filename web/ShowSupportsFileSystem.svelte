b6503346b15b83d7e5a23a29737f781425c110bc
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-02-25 20:28:11 +0000

    gist editor: fix codemirror height

--- web/ShowSupportsFileSystem.svelte
+++ web/ShowSupportsFileSystem.svelte
@@ -7,9 +7,12 @@
 {#if !hasFileSystemSupport}
   <div class="mx-4 mt-2">
     This browser doesn't support <a
+      class="underline"
       href="https://caniuse.com/?search=showDirectoryPicker"
       ><tt>showDirectoryPicker</tt></a
-    > API needed to access files on your computer. Supported browsers: Chrome, Edge,
-    Opera.
+    >
+    API needed to access files on your computer. Supported browsers:
+    <a href="https://www.google.com/chrome/" class="underline">Chrome</a>, Edge,
+    <a href="https://www.opera.com/download" class="underline">Opera</a>.
   </div>
 {/if}
