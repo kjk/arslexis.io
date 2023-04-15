784ac46eb58e33a7a1f6d522f239f74ae09d505c
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-15 02:42:07 -0400

    add GlobalTooltip.svelte

---
+++ web/noted/GlobalTooltip.svelte
@@ -0,0 +1,34 @@
+<script context="module">
+  export function gtooltip(node, html) {
+    let tooltip = document.getElementById("global_tooltip");
+
+    function hide() {
+      tooltip.classList.add("hidden");
+    }
+
+    function show() {
+      tooltip.innerHTML = html;
+      tooltip.classList.remove("hidden");
+    }
+
+    node.addEventListener("mouseenter", show);
+    node.addEventListener("mouseleave", hide);
+
+    return {
+      update(text) {
+        tooltip.innerHTML = text;
+      },
+
+      destroy() {
+        node.removeEventListener("mouseenter", show);
+        node.removeEventListener("mouseleave", hide);
+        hide();
+      },
+    };
+  }
+</script>
+
+<div
+  id="global_tooltip"
+  class="hidden fixed right-1 bottom-1 px-3 py-1 bg-yellow-200 text-gray-700 z-10"
+/>
