89167f210db0cc7f961cb0a6d39895b1c9e20b62
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2023-04-24 01:37:50 -0400

    started on qr decoder

--- web/noted/main.js
+++
@@ -1,9 +0,0 @@
-import "../base.css";
-import "./theme.css";
-
-import App from "./Noted.svelte";
-
-const args = {
-  target: document.getElementById("app"),
-};
-export const app = new App(args);
---
+++ web/qrscanner/qrscanner.svelte
@@ -0,0 +1,73 @@
+<script context="module">
+</script>
+
+<script>
+  import QrScanner from "qr-scanner";
+
+  import Messages from "../Messages.svelte";
+  import { fmtNum, fmtSize, len } from "../util";
+  import { onMount } from "svelte";
+
+  // https://github.com/schmich/instascan
+  let videoElem;
+
+  // let scanner = new Instascan.Scanner({
+  //   video: document.getElementById("scanner"),
+  // });
+
+  // // Listen for QR code scans
+  // scanner.addListener("scan", function (content) {
+  //   console.log("Scanned:", content);
+  //   document.getElementById("result").innerHTML = content;
+  // });
+
+  // // Start the scanner
+  // Instascan.Camera.getCameras()
+  //   .then(function (cameras) {
+  //     if (cameras.length > 0) {
+  //       scanner.start(cameras[0]);
+  //     } else {
+  //       console.error("No cameras found.");
+  //     }
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+
+  onMount(() => {
+    console.log("onMount");
+    let opts = {};
+    const scanner = new QrScanner(
+      videoElem,
+      (result) => {
+        console.log(result);
+      },
+      opts
+    );
+    scanner.start();
+  });
+</script>
+
+<video bind:this={videoElem} class="w-full h-full bg-blue-50" />
+<div id="result" />
+
+<style>
+  video {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  #result {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+</style>
