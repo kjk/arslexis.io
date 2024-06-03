bebf46881c0bb05a038df2ca9b92f6c3fd700a44
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-06-03 15:07:49 +0200

    fm: start on Folder2.svelte and fm2.svelte that uses FileSysDir

---
+++ frontend/src/fm/Folder2.svelte
@@ -0,0 +1,351 @@
+<script context="module">
+  import { FsEntryUpDir } from "../fileutil";
+  import { strCompareNoCase } from "../strutil";
+  /** @typedef {import("../fileutil").FsEntry} FsEntry */
+
+  /**
+   * @param {FsEntry} e
+   * @returns {boolean}
+   */
+  export function isExpanded(e) {
+    return e.getMeta("expanded");
+  }
+
+  /**
+   * @param {FsEntry} e
+   * @param {boolean} expanded
+   */
+  export function setExpanded(e, expanded) {
+    e.setMeta("expanded", expanded);
+  }
+
+  /**
+   * @param {FsEntry[]} entries
+   */
+  export function sortEntries(entries) {
+    /**
+     * @param {FsEntry} e1
+     * @param {FsEntry} e2
+     */
+    function sortFn(e1, e2) {
+      let e1Dir = e1.isDir;
+      let e2Dir = e2.isDir;
+      let name1 = e1.name;
+      let name2 = e2.name;
+      if (e1Dir && e2Dir) {
+        return strCompareNoCase(name1, name2);
+      }
+      if (e1Dir || e2Dir) {
+        return e1Dir ? -1 : 1;
+      }
+      return strCompareNoCase(name1, name2);
+    }
+    entries.sort(sortFn);
+  }
+
+  /**
+   * @param {FsEntry} dirInfo
+   */
+  export function calcDirSizes(dirInfo) {
+    let size = 0;
+    let files = 0;
+    let dirs = 0;
+    let entries = dirInfo.dirEntries;
+    for (let e of entries) {
+      if (e.isDir) {
+        dirs++;
+        let sizes = calcDirSizes(e);
+        size += sizes.size;
+        files += sizes.files;
+        dirs += sizes.dirs;
+      } else {
+        files++;
+        size += e.size;
+      }
+    }
+    dirInfo.size = size;
+    dirInfo.setMeta("size", size);
+    dirInfo.setMeta("files", files);
+    dirInfo.setMeta("dirs", dirs);
+    return { size, files, dirs };
+  }
+</script>
+
+<script>
+  import { fmtNum, fmtSize, len } from "../util";
+  import { tick } from "svelte";
+
+  /** @type {FsEntry} */
+  export let dirRoot;
+  export let isRoot = false;
+  export let indent;
+  /** @type {Function} */
+  export let recalc;
+  /** @type {Function} */
+  export let onSelected;
+  /** @type {Function} */
+  export let onGoUp;
+  export let initialSelectionIdx = 0;
+
+  /** @type {FsEntry[]} */
+  let entries = [];
+  $: calcEntries(dirRoot);
+
+  function calcEntries(dirRoot) {
+    console.log("calcEntries");
+    // SUBTLE: important to not re-order dirInfo.dirEntries because
+    // they could be used in calcLineCount()
+    // we use and sort a copy
+    /** @type {FsEntry[]} */
+    entries = [].concat(dirRoot.dirEntries);
+    sortEntries(entries);
+    if (!isRoot) {
+      let e = new FsEntryUpDir();
+      entries.unshift(e);
+    }
+    if (len(entries) > 0) {
+      tick().then(() => {
+        setSelected(initialSelectionIdx);
+      });
+    }
+  }
+
+  /**
+   * @param {FsEntry} e
+   */
+  function toggleExpand(e) {
+    let expanded = isExpanded(e);
+    setExpanded(e, !expanded);
+    entries = entries;
+  }
+
+  /**
+   * @param {FsEntry} e
+   * @returns {string}
+   */
+  function fmtEntrySize(e) {
+    let s = fmtSize(e.size);
+    if (!e.isDir) {
+      return s;
+    }
+    let meta = e.meta;
+    let s2 = "D: " + fmtNum(meta.dirs);
+    s2 += " F: " + fmtNum(meta.files);
+    return s2 + " " + s;
+  }
+
+  /**
+   * @param {HTMLElement} el
+   * @returns {number}
+   */
+  function findClickedIdx(el) {
+    while (el) {
+      let idxStr = el.getAttribute("data-idx");
+      if (idxStr) {
+        let idx = parseInt(idxStr);
+        // console.log("idx:", idx);
+        return idx;
+      }
+      el = el.parentElement;
+    }
+    return -1;
+  }
+
+  /**
+   * @param {MouseEvent} e
+   */
+  function handleDoubleClicked(e) {
+    let el = /** @type {HTMLElement} */ (e.target);
+    let idx = findClickedIdx(el);
+    let entry = entries[idx];
+    if (entry.isDir) {
+      onSelected(entry, idx);
+    }
+  }
+
+  /**
+   * @param {MouseEvent} e
+   */
+  function handleClicked(e) {
+    let el = /** @type {HTMLElement} */ (e.target);
+    let idx = findClickedIdx(el);
+    setSelected(idx);
+    return;
+  }
+
+  /**
+   * @param {KeyboardEvent} e
+   */
+  function handleKeyDown(e) {
+    let nItems = len(entries);
+    let key = e.key;
+
+    if (key === "Backspace" || key === "ArrowLeft") {
+      e.stopPropagation();
+      e.preventDefault();
+      onGoUp();
+      return;
+    }
+
+    if (key === "Enter" || key === "ArrowRight") {
+      e.stopPropagation();
+      e.preventDefault();
+      let idx = selectedIdx;
+      let entry = entries[idx];
+      if (entry.isDir) {
+        onSelected(entry, idx);
+      }
+      return;
+    }
+
+    if (key === "ArrowUp") {
+      e.stopPropagation();
+      e.preventDefault();
+      let idx = selectedIdx - 1;
+      if (idx < 0) {
+        return;
+      }
+      setSelected(idx);
+      return;
+    }
+
+    if (key === "ArrowDown") {
+      e.stopPropagation();
+      e.preventDefault();
+      let idx = selectedIdx + 1;
+      if (idx >= nItems) {
+        return;
+      }
+      setSelected(idx);
+      return;
+    }
+  }
+
+  let selectedIdx = 0;
+
+  /** @type {HTMLElement} */
+  let tbodyEl;
+
+  /**
+   * @param {number} idx
+   */
+  function setSelected(idx) {
+    let nItems = len(entries);
+    if (idx < 0 || idx >= nItems) {
+      return;
+    }
+    selectedIdx = idx;
+    let q = `[data-idx="${idx}"]`;
+    let el = /** @type {HTMLElement} */ (tbodyEl.querySelector(q));
+    el.focus();
+  }
+</script>
+
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<table
+  class="font-mono text-sm mt-[2px] w-full"
+  on:click={handleClicked}
+  on:dblclick={handleDoubleClicked}
+  on:keydown={handleKeyDown}
+>
+  <tbody bind:this={tbodyEl}>
+    {#each entries as e, idx}
+      {#if e.isDir}
+        <tr
+          data-idx={idx}
+          tabindex="0"
+          class="hover:bg-gray-200 hover:cursor-pointer"
+        >
+          <td class="ind-{indent} font-semibold">
+            {#if isExpanded(e)}
+              ▼
+            {:else}
+              ▶
+            {/if}
+            {e.name}
+          </td>
+          <td class="pl-2 text-right whitespace-nowrap">
+            {fmtEntrySize(e)}
+          </td>
+        </tr>
+        {#if isExpanded(e)}
+          <svelte:self {recalc} dirRoot={e} indent={indent + 1} />
+        {/if}
+      {/if}
+    {/each}
+
+    {#each entries as e, idx (idx)}
+      {#if !e.isDir}
+        <tr
+          data-idx={idx}
+          tabindex="0"
+          class="hover:bg-gray-200 even:bg-gray-50"
+        >
+          <td class="ind-{indent + 1}">{e.name} </td>
+          <td class="pl-2 text-right whitespace-nowrap">{fmtEntrySize(e)} </td>
+        </tr>
+      {/if}
+    {/each}
+  </tbody>
+</table>
+
+<style>
+  /*
+  TODO: hacky because there can be more indentation levels
+  */
+  :global(.ind-1) {
+    padding-left: 0.5rem;
+  }
+  :global(.ind-2) {
+    padding-left: 1rem;
+  }
+  :global(.ind-3) {
+    padding-left: 1.5rem;
+  }
+  :global(.ind-4) {
+    padding-left: 2rem;
+  }
+  :global(.ind-5) {
+    padding-left: 2.5rem;
+  }
+  :global(.ind-6) {
+    padding-left: 3rem;
+  }
+  :global(.ind-7) {
+    padding-left: 3.5rem;
+  }
+  :global(.ind-8) {
+    padding-left: 4rem;
+  }
+  :global(.ind-9) {
+    padding-left: 4.5rem;
+  }
+  :global(.ind-10) {
+    padding-left: 5rem;
+  }
+  :global(.ind-11) {
+    padding-left: 5.5rem;
+  }
+  :global(.ind-12) {
+    padding-left: 6rem;
+  }
+  :global(.ind-13) {
+    padding-left: 6.5rem;
+  }
+  :global(.ind-14) {
+    padding-left: 7rem;
+  }
+  :global(.ind-15) {
+    padding-left: 7.5rem;
+  }
+  :global(.ind-16) {
+    padding-left: 8rem;
+  }
+  :global(.ind-17) {
+    padding-left: 8.5rem;
+  }
+
+  tr:focus {
+    @apply bg-blue-100;
+  }
+</style>
---
+++ frontend/src/fm/fm2.svelte
@@ -0,0 +1,283 @@
+<script context="module">
+</script>
+
+<script>
+  import TopNav from "../TopNav.svelte";
+  import Folder, { calcDirSizes } from "./Folder2.svelte";
+  import Messages from "../Messages.svelte";
+  import Progress, { progress } from "../Progress.svelte";
+  import ShowSupportsFileSystem from "../ShowSupportsFileSystem.svelte";
+  import { recent } from "./fmstore";
+  import {
+    verifyHandlePermission,
+    supportsFileSystem,
+    readDirRecur,
+    forEachFsEntry,
+  } from "../fileutil";
+  import { logFmEvent } from "../events";
+  import { len } from "../util";
+
+  /** @typedef {import("./fmstore").RecentEntry} RecentEntry */
+  /** @typedef {import("../fileutil").FsEntry} FsEntry */
+
+  class FsEntryWithIndex {
+    entry;
+    idx;
+    /**
+     * @param {FsEntry} e
+     * @param {number} i
+     */
+    constructor(e, i) {
+      this.entry = e;
+      this.idx = i;
+    }
+  }
+
+  /** @type {FsEntryWithIndex[]} */
+  let dirPath = [];
+
+  /** @type {FsEntry} */
+  let dirRoot = null;
+  $: calcDirRoot(dirPath);
+  let initialSelectionIdx = 0;
+
+  function calcDirRoot(dirPath) {
+    console.log("calcDirRoot:", dirPath);
+    let n = len(dirPath);
+    if (n > 0) {
+      dirRoot = dirPath[n - 1].entry;
+      return;
+    }
+    dirRoot = null;
+  }
+
+  // /** @type {boolean} */
+  // let isShowingFiles = false;
+  // $: isShowngFiles = dirRoot != null;
+
+  $progress = "";
+
+  let hasFileSystemSupport = supportsFileSystem();
+
+  let defaultExcludeDirs = [".git", "node_modules", "dist"];
+  let defaultExcludeFiles = ["package-lock.json", "yarn.lock"];
+
+  /**
+   * @param {FsEntry} entry
+   */
+  function shouldExclude(entry) {
+    let name = entry.name.toLowerCase();
+    let exclude;
+    if (entry.isDir) {
+      exclude = defaultExcludeDirs.includes(name);
+    } else {
+      exclude = defaultExcludeFiles.includes(name);
+    }
+    // if (exclude) {
+    //   setExcluded(entry, exclude);
+    // }
+    return exclude;
+  }
+
+  /**
+   * @param {FileSystemDirectoryHandle} dh
+   */
+  async function addToRecent(dh) {
+    // note: no way to check for duplicates
+    // dirHandle for a directory doesn't compare equal
+    // to another dirHandle for the same directory
+    // we can't filter by name becaues multiple directories
+    // on disk can have the same name
+    let e = {
+      dirHandle: dh,
+      name: dh.name,
+    };
+    $recent.push(e);
+    $recent = $recent;
+  }
+
+  async function removeFromRecent(idx) {
+    $recent.splice(idx, 1);
+    $recent = $recent;
+  }
+
+  /**
+   * @param {string} dir
+   * @returns {boolean}
+   */
+  function shouldSkipEntry(entry, dir) {
+    // use it to show progress
+    if (entry.kind === "directory") {
+      $progress = `Reading ${dir}`;
+    }
+    return false;
+  }
+
+  /**
+   * @param {FileSystemDirectoryHandle} dirHandle
+   */
+  async function openDirectory(dirHandle) {
+    await verifyHandlePermission(dirHandle, false);
+    dirPath = [];
+    $progress = "Reading directory entries...";
+    let di;
+    try {
+      di = await readDirRecur(dirHandle, shouldSkipEntry, "");
+    } catch (e) {
+      console.log("error reading dir", e);
+      // can fail if e.g. saved directory was deleted
+      $progress = "";
+      return;
+    }
+    logFmEvent("openDir");
+    forEachFsEntry(di, shouldExclude);
+    calcDirSizes(di);
+    dirPath = [new FsEntryWithIndex(di, 0)];
+    $progress = "";
+  }
+
+  /**
+   * @param {FileSystemDirectoryHandle} dirHandle
+   */
+  async function openRecentDir(dirHandle) {
+    try {
+      await verifyHandlePermission(dirHandle, false);
+    } catch {
+      // didn't get permission
+      return;
+    }
+    openDirectory(dirHandle);
+  }
+
+  async function openFolder() {
+    /** @type {FileSystemDirectoryHandle} */
+    let dirHandle;
+    try {
+      // @ts-ignore
+      dirHandle = await window.showDirectoryPicker();
+    } catch {
+      // dismissed dialog or no permissions
+      return;
+    }
+    addToRecent(dirHandle);
+    openDirectory(dirHandle);
+  }
+
+  async function recalc() {
+    calcDirSizes(dirRoot);
+    // console.log("finished calcDIrSizes");
+    dirRoot = dirRoot;
+  }
+  /**
+   * @param {FsEntry} dirEntry
+   */
+  function handleSelected(dirEntry, idx) {
+    if (dirEntry.name === "..") {
+      handleGoUp();
+      return;
+    }
+    let ei = new FsEntryWithIndex(dirEntry, idx);
+    dirPath.push(ei);
+    dirPath = dirPath;
+    initialSelectionIdx = 0;
+  }
+
+  function handleGoUp() {
+    if (len(dirPath) > 1) {
+      let el = dirPath.pop();
+      initialSelectionIdx = el.idx;
+      dirPath = dirPath;
+    }
+  }
+</script>
+
+<div class="h-screen fixed inset-0 flex flex-col overflow-y-hidden">
+  <TopNav>
+    <span class="text-purple-800">File Manager in the browser</span>
+    {#if dirRoot}
+      <button
+        class="border ml-2 text-sm border-gray-500 px-2 py-0.5 hover:bg-gray-100"
+        on:click={openFolder}>Open another folder</button
+      >
+      {#if len($recent) > 0}
+        <div class="flex ml-4 text-sm">
+          <div>recent:</div>
+          {#each $recent as e}
+            <button
+              class="ml-2 underline"
+              on:click={() => openRecentDir(e.dirHandle)}>{e.name}</button
+            >{/each}
+        </div>
+      {/if}
+    {/if}
+  </TopNav>
+
+  {#if hasFileSystemSupport}
+    {#if !dirRoot}
+      <div class="flex items-baseline mx-4 mt-2 mb-2">
+        <button
+          class="border border-gray-500 px-2 py-0.5 hover:bg-gray-100"
+          on:click={openFolder}>Open folder</button
+        >
+        <div class="ml-2">from your computer to browse it.</div>
+      </div>
+    {/if}
+  {:else}
+    <ShowSupportsFileSystem />
+  {/if}
+
+  {#if !dirRoot}
+    {#if len($recent) > 0}
+      <div class="ml-4 mt-2 mb-2">
+        <div>Recently opened:</div>
+        <table class="table-auto ml-4">
+          {#each $recent as e, idx}
+            <tr>
+              <td class="px-1">{e.name}</td>
+              <td class="px-1"
+                ><button
+                  class="underline hover:cursor-pointer"
+                  on:click={() => openRecentDir(e.dirHandle)}>open</button
+                ></td
+              >
+              <td class="px-1">
+                <button
+                  class="underline hover:cursor-pointer"
+                  on:click={() => removeFromRecent(idx)}>remove</button
+                >
+              </td>
+            </tr>
+          {/each}
+        </table>
+      </div>
+    {/if}
+  {/if}
+
+  {#if dirRoot}
+    <div class="flex font-bold font-mono text-sm ml-3">
+      {#each dirPath as e}
+        <div class="ml-1">{e.entry.name}</div>
+        <div class="ml-1">/</div>
+      {/each}
+    </div>
+    {#key dirRoot}
+      <div class="overflow-y-scroll h-min-0 h-full ml-2">
+        <Folder
+          {initialSelectionIdx}
+          {recalc}
+          {dirRoot}
+          isRoot={len(dirPath) == 1}
+          indent={0}
+          onSelected={handleSelected}
+          onGoUp={handleGoUp}
+        />
+      </div>
+    {/key}
+  {/if}
+</div>
+
+<div class="mt-4" />
+
+<Progress />
+
+<Messages />
