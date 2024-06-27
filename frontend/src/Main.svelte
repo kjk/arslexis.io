f3a94a447b95776be48e93e098f828c9f88da936
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2024-06-27 15:34:22 +0200

    tweak design of main page; add link to SumatraPDF

--- frontend/src/Main.svelte
+++ frontend/src/Main.svelte
@@ -1,3 +1,5 @@
+<svelte:options runes={true} />
+
 <script>
   let host = location.host; // localhost:9301 or arslexis.io
   function isLocal() {
@@ -5,16 +7,8 @@
   }
 </script>
 
-<div class="flex items-center justify-between mx-4 mt-2 fixed top-full right-0">
+<div class="flex items-center justify-between mx-4 mt-2 fixed right-0">
   <div class="flex">
-    <a
-      class="px-1 py-1 mr-1 hover:underline text-xs"
-      href="https://github.com/kjk/tools.arslexis.io/discussions"
-      target="_blank"
-      rel="noreferrer"
-    >
-      feedback
-    </a>
     <a
       class="px-1 py-1 mr-1 hover:bg-gray-100"
       href="https://github.com/kjk/tools.arslexis.io"
@@ -81,15 +75,19 @@
 <div class="min-h-screen flex flex-col">
   <div class="flex flex-col items-center pt-2 pb-4">
     <div
-      class="text-6xl font-bold outlined-text"
+      class="text-6xl font-bold outlined-text !outline-blue-700"
       style="font-family: 'Cooper Black', 'Arial Rounded MT Bold', 'Futura', 'Roboto', sans-serif;"
     >
-      <a href="https://arslexis.io">ArsLexis</a>
+      <a
+        href="https://arslexis.io"
+        target="_blank"
+        title="ArsLexis Website"
+        class="!no-underline !text-white">ArsLexis</a
+      >
     </div>
     <!-- <img src={logo} alt="ArsLexis Logo" width="220" /> -->
     <div class="text-sm text-gray-600 mt-2">
       independent software company by creator of <a
-        class="underline"
         target="_blank"
         href="https://www.sumatrapdfreader.org/free-pdf-reader">SumatraPDF</a
       >
@@ -97,88 +95,58 @@
   </div>
 
   <div class="text-center">
-    Tools in the browser by <a
-      class="underline text-gray-600"
-      href="https://arslexis.io">ArsLexis</a
-    >
+    Software by <a class="text-gray-600" href="https://arslexis.io">ArsLexis</a>
   </div>
+
+  {#snippet row(name, url, desc)}
+    <tr>
+      <td class="text-left align-top"
+        ><a class="font-semibold whitespace-nowrap" href={url}>{name}</a>
+      </td>
+      <td class="pl-4 align-top">{@html desc}</td>
+    </tr>
+  {/snippet}
+
+  {#snippet tbody()}
+    {@render row(
+      "Edna",
+      "https://edna.arslexis.io",
+      "a note taker / scratchpad for developers and power users",
+    )}
+    {@render row(
+      "SumatraPDF",
+      "https://www.sumatrapdfreader.org",
+      "small, fast PDF / eBook / comic book reader for Windows",
+    )}
+    {@render row(
+      "Gist Editor",
+      "/gisteditor/",
+      "GitHub Gist editor and a notepad for notes and code snippets stored locally in the browser",
+    )}
+    {@render row(
+      "notepad2",
+      "/notepad2/",
+      `like a Windows <a
+          class="whitespace-nowrap"
+          target="_blank"
+          href="https://github.com/zufuliu/notepad2">notepad2</a
+        >
+        text editor but in the browser`,
+    )}
+    {@render row("Go Playground", "/goplayground/", "a better Go Playground")}
+    <!-- {@render row(
+      "calc",
+      "/calc/",
+      "calculator as a notepad (like Numi or Soulver)",
+    )} -->
+    {@render row("unzip", "/unzip/", "unzip a file in the browser")}
+    {@render row("wc", "/wc/", "like <tt>wc</tt>, but in the browser")}
+    {@render row("File Manager", "/fm/", "file manager, but in the browser")}
+  {/snippet}
 
   <table class="table-auto mx-8 mt-2 striped-tr">
     <tbody>
-      <tr>
-        <td class="text-left align-top"
-          ><a
-            class="underline whitespace-nowrap"
-            href="https://edna.arslexis.io">Edna</a
-          >
-        </td>
-        <td class="pl-4 align-top"
-          >a scratchpad / note taker for developers and race car drivers</td
-        >
-      </tr>
-
-      <tr>
-        <td class="text-left align-top">
-          <a class="underline whitespace-nowrap" href="/gisteditor/"
-            >Gist Editor</a
-          >
-        </td>
-        <td class="pl-4 align-top"
-          >GitHub Gist editor and a notepad for notes and code snippets stored
-          locally in the browser</td
-        >
-      </tr>
-
-      <tr>
-        <td class="text-left align-top"
-          ><a class="underline whitespace-nowrap" href="/notepad2/">notepad2</a>
-        </td>
-        <td class="pl-4 align-top"
-          >like a Windows <a
-            class="underline whitespace-nowrap"
-            target="_blank"
-            href="https://github.com/zufuliu/notepad2">notepad2</a
-          >
-          text editor but in the browser</td
-        >
-      </tr>
-
-      <tr>
-        <td><a class="underline" href="/goplayground/">Go Playground</a></td>
-        <td class="pl-4 align-top">a better Go Playground</td>
-      </tr>
-
-      <!--
-    <tr>
-      <td class="text-left align-top">
-        <a class="underline whitespace-nowrap" href="/calc/">calc</a>
-      </td>
-      <td class="pl-4 align-top"
-        >calculator as a notepad (like Numi or Soulver)</td
-      >
-    </tr>
-    -->
-
-      <tr>
-        <td class="text-left align-top">
-          <a class="underline whitespace-nowrap" href="/unzip/">unzip</a>
-        </td>
-        <td class="pl-4 align-top">unzip a file in the browser</td>
-      </tr>
-
-      <tr>
-        <td class="text-left align-top">
-          <a class="underline whitespace-nowrap" href="/wc/">wc</a>
-        </td>
-        <td class="pl-4 align-top">like <tt>wc</tt>, but in the browser</td>
-      </tr>
-
-      <tr>
-        <td class="text-left align-top">
-          <a class="underline whitespace-nowrap" href="/fm/">File Manager</a>
-        </td>
-        <td class="pl-4 align-top">file manager, but in the browser</td>
-      </tr>
+      {@render tbody()}
     </tbody>
   </table>
 
@@ -191,28 +159,13 @@
 
     <table class="table-auto mx-8 mt-2">
       <tbody>
-        <tr>
-          <td class="text-left align-top">
-            <a class="underline whitespace-nowrap" href="/reader/">reader</a>
-          </td>
-          <td class="pl-4 align-top">Comic Book (.cbz, .cbr) reader</td>
-        </tr>
-
-        <tr>
-          <td
-            ><a class="underline" href="/image-resize-optimize/"
-              >image resize and optimize</a
-            ></td
-          >
-          <td class="pl-4 align-top"
-            >optimize and resize multiple images at once</td
-          >
-        </tr>
-
-        <tr>
-          <td><a class="underline" href="/qrscanner/">qrscanner</a></td>
-          <td class="pl-4 align-top">qrscanner</td>
-        </tr>
+        {@render row("reader", "/reader/", "Comic Book (.cbz, .cbr) reader")}
+        {@render row(
+          "image resize and optimize",
+          "/image-resize-optimize/",
+          "optimize and resize multiple images at once",
+        )}
+        {@render row("qrscanner", "/qrscanner/", "qrscanner")}
       </tbody>
     </table>
   {/if}
@@ -220,41 +173,36 @@
   <div class="mt-8 mx-8 flex gap-2">
     <div>Have feedback?</div>
     <a
-      class="underline"
       href="https://github.com/kjk/tools.arslexis.io/discussions"
       target="_blank">discuss</a
     >
     <div>&bull;</div>
-    <a
-      class="underline"
-      href="https://github.com/kjk/tools.arslexis.io/issues"
-      target="_blank">request features</a
+    <a href="https://github.com/kjk/tools.arslexis.io/issues" target="_blank"
+      >request features</a
     >
     <div>&bull;</div>
-    <a
-      class="underline"
-      href="https://github.com/kjk/tools.arslexis.io/issues"
-      target="_blank">report bugs</a
+    <a href="https://github.com/kjk/tools.arslexis.io/issues" target="_blank"
+      >report bugs</a
     >
     <div>&bull;</div>
-    <a
-      class="underline"
-      href="https://blog.kowalczyk.info/contactme"
-      target="_blank">contact me</a
+    <a href="https://blog.kowalczyk.info/contactme" target="_blank"
+      >contact me</a
     >
   </div>
   <div class="grow"></div>
   <hr class="mt-4" />
   <div class="text-center my-2">
-    Made by&nbsp;<a
-      class="underline"
-      target="_blank"
-      href="https://blog.kowalczyk.info">Krzysztof Kowalczyk</a
+    Made by&nbsp;<a target="_blank" href="https://blog.kowalczyk.info"
+      >Krzysztof Kowalczyk</a
     >
   </div>
 </div>
 
 <style>
+  a {
+    @apply underline underline-offset-2;
+    @apply text-blue-700;
+  }
   .outlined-text {
     color: rgb(248 250 252);
     color: #fff;
@@ -263,8 +211,15 @@
       1px -1px 0 #000,
       -1px 1px 0 #000,
       1px 1px 0 #000;
+    /* text-shadow:
+      -1px -1px 0 #00f,
+      1px -1px 0 #00f,
+      -1px 1px 0 #00f,
+      1px 1px 0 #00f; */
   }
+  /*
   tr:nth-child(odd) {
     @apply bg-slate-50;
   }
+    */
 </style>
