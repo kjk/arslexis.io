e94639ac08e7fadac86bcfdbc87447b5ac79aff7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2025-03-12 16:09:08 +0100

    start on importing bookmarks

--- frontend/src/bookmarks/bookmarks.svelte
+++ frontend/src/bookmarks/bookmarks.svelte
@@ -1,6 +1,30 @@
 <svelte:options runes={true} />
 
 <script>
+  import { onMount } from "svelte";
+
+  /**
+   @typedef {{
+    url: string,
+    name: string,
+    tags: string[],
+   }} Bookmark
+  */
+
+  function readBookmarksFromLS() {
+    let key = "b:bookmarks.json";
+  }
+  onMount(() => {});
+
+  function importBookmarks() {
+    console.log("import bookmarks");
+  }
 </script>
 
-<div>Hello to bookmarks</div>
+<div class="flex flex-col">
+  <div>Hello to bookmarks</div>
+  <button
+    class="border border-gray-400 px-2 py-1 hover:bg-gray-300 self-center"
+    onclick={importBookmarks}>Import bookmarks</button
+  >
+</div>
